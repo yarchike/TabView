488e9b0db6f1c8a784ce0bc1270b39e4900b31b0
Yarchike <yarchike@gmail.com>
2021-12-23 19:26:02 +0300

    init

--- UiTabView/ViewController.swift
+++ UiTabView/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var myTableView = UITableView()
+    let indentifire  = "MyCell"
+    var array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createTable()
     }
-
-
+    func createTable()
+    {
+        self.myTableView = UITableView(frame: view.bounds, style: .plain)
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: indentifire)
+        
+        self.myTableView.delegate =  self
+        self.myTableView.dataSource = self
+            
+        myTableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        
+        view.addSubview(myTableView)
+    }
+    
+    //UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: indentifire, for: indexPath)
+        
+        let number = array[indexPath.row]
+        
+        cell.textLabel?.text = number
+        
+        return cell
+    }
+    //UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+//    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+//        print("Accesory patch =", indexPath)
+//        let ounerCell = tableView.cellForRow(at: indexPath)
+//        print("cell title =", ounerCell?.textLabel?.text ?? "nil")
+//    }
+    
 }
 
